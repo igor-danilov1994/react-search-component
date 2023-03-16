e9d81bf1a1e87870cf87308827d0a8bf8c3ab3e7
igor <igor.danilov1824@gmail.com>
2023-03-16 13:44:15 +0200

    run deploy update step 4

---
+++ src/components/shared/inputs/input/Input.tsx
@@ -0,0 +1,37 @@
+import { FC, useEffect, useState } from 'react';
+
+import { useDebounce } from 'hooks/useDebounce';
+
+interface IProps {
+  value: string;
+  onChange: (value: string) => void;
+  placeholder: string;
+}
+
+export const Input: FC<IProps> = (props) => {
+  const { value = '', onChange, placeholder } = props
+  const [inputValue, setInputValue] = useState(value);
+  const searchQuery = useDebounce(inputValue, 1000);
+
+  const onChangeHandler = (value: string) => {
+    setInputValue(value);
+
+    if (!value.length){
+      onChange('')
+    }
+  };
+
+  useEffect( () => {
+    if (searchQuery){
+      onChange(searchQuery)
+    }
+  }, [searchQuery] )
+
+  return (
+      <input
+        value={inputValue ?? ''}
+        onChange={(e) => onChangeHandler(e.target.value)}
+        placeholder={placeholder}
+      />
+  );
+};
